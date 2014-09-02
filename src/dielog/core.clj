87b2346cb4607b0755eb9a62c449d4c9555eefa8
Vic Goldfeld <vic@longstorm.org>
2014-09-02 19:48:31 -0300

    initial commit

---
+++ src/dielog/core.clj
@@ -0,0 +1,6 @@
+(ns dielog.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/dielog/core_test.clj
@@ -0,0 +1,7 @@
+(ns dielog.core-test
+  (:require [clojure.test :refer :all]
+            [dielog.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
